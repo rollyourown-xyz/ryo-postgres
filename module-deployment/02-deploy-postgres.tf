73cf8804a38e686a22473e99a37a1efcc1cb23b5
xyzroller <xyzroller@rollyourown.xyz>
2022-05-18 19:40:26 +0200

    Change rollyourown.xyz to rollyourown in text

--- module-deployment/02-deploy-postgres.tf
+++ module-deployment/02-deploy-postgres.tf
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2022 Wilfred Nicoll <xyzroller@rollyourown.xyz>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Deploy PostgreSQL for rollyourown.xyz projects
+# Deploy PostgreSQL for rollyourown projects
 ################################################
 
 ### Deploy postgres container
